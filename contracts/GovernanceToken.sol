d445a960d711003d8f17b1fdd6432ae1840b2bfd
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-27 14:45:08 +0500

    mint GovernanceToken in the constructor.

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -9,10 +9,14 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
  * * This token is designed to grant voting power to accounts, and it extends the ERC20Votes standard in order to facilitate the taking of snapshots. This feature is crucial, as it provides protection against double voting. It should be noted that this token is not a standard ERC20 token, as it includes additional functionality beyond the ERC20 standard.
  */
 contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000;
+    
     constructor()
         ERC20("GovernanceToken", "GT")
         ERC20Permit("GovernanceToken")
-    {}
+    {
+        _mint(msg.sender, s_maxSupply);
+    }
 
     // The functions below are overrides required by Solidity.
 
